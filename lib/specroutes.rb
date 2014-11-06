7e582d0c91827e36e6cd8803b6e1dc9021d1c2da
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-11-06 12:09:38 +0100

    add top-level call to define routes.

--- lib/specroutes.rb
+++ lib/specroutes.rb
@@ -1,4 +1,8 @@
 module Specroutes
+  def self.define(rails_router, &block)
+    routes = Specroutes::Routing::Routes.new(rails_router)
+    routes.draw(&block)
+  end
 end
 
 require 'specroutes/utility_belt'
