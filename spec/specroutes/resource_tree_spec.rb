be519c6b4b3f75515bf5c9e7884f4b29f5fa3d3e
Tim Reddehase <tredd@informatik.uni-bremen.de>
2015-01-09 00:07:45 +0100

    add specs for resource tree instance methods.

--- spec/specroutes/resource_tree_spec.rb
+++ spec/specroutes/resource_tree_spec.rb
@@ -18,4 +18,34 @@ describe Specroutes::ResourceTree do
         to_not raise_error
     end
   end
+
+  context '#depth' do
+    it 'should be zero for the root-node' do
+      expect(tree.depth).to eq(0)
+    end
+
+    it 'should be 1 for the first child-node' do
+      expect(foo_child.depth).to eq(1)
+    end
+
+    it 'should equal the one of a sibling' do
+      expect(foo_child.depth).to eq(bar_child.depth)
+    end
+  end
+
+  context '#path' do
+    it 'should be the path_portion on the root node' do
+      expect(tree.path).to eq(tree.path_portion)
+    end
+
+    it 'should be the correct path for a child node' do
+      expect(foo_child.path).to eq('/foo')
+    end
+  end
+
+  context '#children' do
+    it 'should have the correct children on the root node' do
+      expect(tree.children).to include(foo_child, bar_child)
+    end
+  end
 end
