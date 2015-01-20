25b1117b8fd594ea1df99b4a6db3575059d3da3f
Tim Reddehase <tredd@informatik.uni-bremen.de>
2015-01-20 15:21:17 +0100

    take a method_missing approach to route-delegation
    Instead of caching the method calls in order to traverse
    and altering them before execution, we'll use a method_missing
    approach in order to delegate to the rails router's mapper.
    
    This should be more flexible wrt. monkey-patching gems like devise or to
    nested-block-syntax like resources-collection.

--- lib/specroutes/routing/routes.rb
+++ lib/specroutes/routing/routes.rb
@@ -59,10 +59,32 @@ module Specroutes::Routing
     def draw(&block)
       rails_router.clear!
       instance_eval(&block)
-      execute_methods
       rails_router.finalize!
       specification.register_with_application
       nil
+    end
+
+    private
+    def method_missing(method_name, *args, &block)
+      if mapper.respond_to?(method_name)
+        mapped_exec { send(method_name, *args, &block) }
+      else
+        raise NoMethodError.new <<-ERR
+#{method_name} is neither defined for #{self.class} nor for #{mapper.class}.
+        ERR
+      end
+    end
+
+    def mapped_exec(&block)
+      if rails_router.default_scope
+        mapper.with_default_scope(rails_router.default_scope, &block)
+      else
+        mapper.instance_exec(&block)
+      end
+    end
+
+    def mapper
+      @mapper ||= ActionDispatch::Routing::Mapper.new(rails_router)
     end
 
   end
